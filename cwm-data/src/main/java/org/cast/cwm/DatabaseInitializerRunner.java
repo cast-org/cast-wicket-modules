3ce439841d6023f99677f98103a133ecaba0f53c
lmccormack <lmccormack@users.noreply.github.com>
2012-05-23 12:30:39 +0000

    added license info

--- cwm-data/src/main/java/org/cast/cwm/DatabaseInitializerRunner.java
+++ cwm-data/src/main/java/org/cast/cwm/DatabaseInitializerRunner.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011 CAST, Inc.
+ *
+ * This file is part of the UDL Curriculum Toolkit:
+ * see <http://code.google.com/p/udl-curriculum-toolkit>.
+ *
+ * The UDL Curriculum Toolkit is free software: you can redistribute and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The UDL Curriculum Toolkit is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cast.cwm;
 
 import java.util.List;
@@ -8,7 +27,6 @@ import net.databinder.hib.SessionUnit;
 
 import org.apache.wicket.injection.web.InjectorHolder;
 import org.cast.cwm.data.init.IDatabaseInitializer;
-import org.cast.cwm.service.CwmService;
 import org.cast.cwm.service.ICwmService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- cwm-data/src/main/java/org/cast/cwm/service/ICwmService.java
+++ cwm-data/src/main/java/org/cast/cwm/service/ICwmService.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011 CAST, Inc.
+ *
+ * This file is part of the UDL Curriculum Toolkit:
+ * see <http://code.google.com/p/udl-curriculum-toolkit>.
+ *
+ * The UDL Curriculum Toolkit is free software: you can redistribute and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The UDL Curriculum Toolkit is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cast.cwm.service;
 
 import java.util.List;
