44b32d134b7a181049d856d48076b50f315de52c
bgoldowsky@gmail.com <bgoldowsky@gmail.com@ff44da35-2226-727e-5b45-4a5c19511d84>
2012-11-29 15:23:04 +0000

    DialogBorder improvements:  - now includes a move icon (but it is not shown by default).  - visibility of close icon can be turned off.  - copied in CSS file from visural project, dropping dependency on that library.  - title will be set invisible when empty  - modalInit() method, if defined, will be called after display.

--- trunk/cwm-data/src/main/resources/org/cast/cwm/data/component/DialogBorder.js
+++ trunk/cwm-data/src/main/resources/org/cast/cwm/data/component/DialogBorder.js
@@ -9,7 +9,7 @@ jQuery.fn.center = function() {
 	this.css("position","absolute");
 	this.css("top",(jQuery(window).height()-this.height())/2+jQuery(window).scrollTop()+"px");
 	this.css("left",(jQuery(window).width()-this.width())/2+jQuery(window).scrollLeft()+"px");
-	return this
+	return this;
 };
 
 /**
