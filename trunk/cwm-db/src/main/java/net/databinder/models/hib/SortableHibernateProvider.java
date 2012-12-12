2f2dc28edb0f16607d9f50e206c6996ada99cb89
bgoldowsky@gmail.com <bgoldowsky@gmail.com@ff44da35-2226-727e-5b45-4a5c19511d84>
2012-12-12 21:40:05 +0000

    Make all Databinder license statements consistent with each other; allow this as alternate license header when checking

--- trunk/cwm-db/src/main/java/net/databinder/models/hib/SortableHibernateProvider.java
+++ trunk/cwm-db/src/main/java/net/databinder/models/hib/SortableHibernateProvider.java
@@ -1,9 +1,7 @@
-package net.databinder.models.hib;
-
 /*
  * Databinder: a simple bridge from Wicket to Hibernate
- * Copyright (C) 2006  Nathan Hamblen nathan@technically.us
-
+ * Copyright (C) 2008  Nathan Hamblen nathan@technically.us
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -18,6 +16,7 @@ package net.databinder.models.hib;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+package net.databinder.models.hib;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.*;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.*;
