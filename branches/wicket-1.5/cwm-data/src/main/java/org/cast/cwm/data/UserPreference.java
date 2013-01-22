f54f18d1baae0c0bbd7c3536c289fb24d885e2ea
bgoldowsky@gmail.com <bgoldowsky@gmail.com@ff44da35-2226-727e-5b45-4a5c19511d84>
2013-01-22 21:01:20 +0000

    License headers and pom tweak

--- branches/wicket-1.5/cwm-data/src/main/java/org/cast/cwm/data/UserPreference.java
+++ branches/wicket-1.5/cwm-data/src/main/java/org/cast/cwm/data/UserPreference.java
@@ -1,15 +1,15 @@
 /*
- * Copyright 2011 CAST, Inc.
+ * Copyright 2011-2013 CAST, Inc.
  *
- * This file is part of the UDL Curriculum Toolkit:
- * see <http://code.google.com/p/udl-curriculum-toolkit>.
+ * This file is part of the CAST Wicket Modules:
+ * see <http://code.google.com/p/cast-wicket-modules>.
  *
- * The UDL Curriculum Toolkit is free software: you can redistribute and/or
- * modify it under the terms of the GNU Affero General Public License as
+ * The CAST Wicket Modules are free software: you can redistribute and/or
+ * modify them under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * The UDL Curriculum Toolkit is distributed in the hope that it will be
+ * The CAST Wicket Modules are distributed in the hope that they will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Affero General Public License for more details.
