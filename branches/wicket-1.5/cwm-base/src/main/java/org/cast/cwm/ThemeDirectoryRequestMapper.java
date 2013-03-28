2f01486ec8a902217e630c59cef8e24531d11dcd
bgoldowsky@gmail.com <bgoldowsky@gmail.com@ff44da35-2226-727e-5b45-4a5c19511d84>
2013-03-28 15:33:45 +0000

    Try this instead of ResourceDirectory - implemented as a request mapper. May work better with multi-folder-level URLs.

---
+++ branches/wicket-1.5/cwm-base/src/main/java/org/cast/cwm/ThemeDirectoryRequestMapper.java
@@ -0,0 +1,118 @@
+package org.cast.cwm;
+
+import java.io.File;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.markup.html.IPackageResourceGuard;
+import org.apache.wicket.request.IRequestHandler;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.handler.resource.ResourceStreamRequestHandler;
+import org.apache.wicket.request.mapper.AbstractMapper;
+import org.apache.wicket.request.resource.PackageResource.PackageResourceBlockedException;
+import org.apache.wicket.settings.IResourceSettings;
+import org.apache.wicket.util.resource.FileResourceStream;
+import org.apache.wicket.util.resource.IResourceStream;
+import org.apache.wicket.util.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A request mapper for static files in a filesystem directory.
+ * It will response to any request that starts with a given set of prefixes, for example you 
+ * might set up CSS and JS files to be served from a fixed location as follows:
+ * <code><pre>
+ *   getRootRequestMapperAsCompound().add(new ThemeDirectoryRequestMapper(themeDir, "js", "css"));
+ * </pre></code>
+ * 
+ * Cache duration for files can be set, will default to {@link IResourceSettings#getDefaultCacheDuration()}.
+ * Files are checked against the PackageResourceGuard before being delivered.
+ *  
+ * @author bgoldowsky
+ *
+ */
+public class ThemeDirectoryRequestMapper extends AbstractMapper {
+
+	private final String themeDirectory;
+	
+	private final String[] prefixes;
+	
+	private Duration cacheDuration = null;
+	
+	protected static final int COMPATIBILITY_SCORE = 7;
+
+	private static final Logger log = LoggerFactory.getLogger(ThemeDirectoryRequestMapper.class);
+
+	public ThemeDirectoryRequestMapper(File themeDirectory, String... prefixes) {
+		super();
+		this.themeDirectory = themeDirectory.getAbsolutePath();
+		if (prefixes == null)
+			throw new IllegalArgumentException("List of prefixes cannot be empty");
+		this.prefixes = prefixes;
+	}
+
+	// Mapper is compatible with any URL that starts with one of the prefixes
+	public int getCompatibilityScore(Request request) {
+		String url = request.getUrl().toString();
+		for (String prefix : prefixes)
+			if (url.startsWith(prefix))
+				return COMPATIBILITY_SCORE;
+		return 0;
+	}
+
+	public IRequestHandler mapRequest(Request request) {
+		String url = request.getUrl().toString();
+		for (String prefix : prefixes) {
+			if (url.startsWith(prefix)) {
+				IResourceStream stream = getResourceStream(themeDirectory + "/" + url);
+				if (stream==null)
+					return null;
+				ResourceStreamRequestHandler handler = new ResourceStreamRequestHandler(stream);
+				if (cacheDuration != null)
+					handler.setCacheDuration(cacheDuration);
+				return handler;
+			}
+		}
+		return null;
+	}
+
+	public Url mapHandler(IRequestHandler requestHandler) {
+		// TODO can we reverse-map requests like this?   Perhaps if we generate something more sophisticated than the ResourceStreamRequestHandler?
+		return null;
+	}
+	
+	public IResourceStream getResourceStream (String absolutePath) {
+		if (accessCheck(absolutePath) == false) {
+			throw new PackageResourceBlockedException(
+					"Access denied to (static) package resource "
+							+ absolutePath + ". See IPackageResourceGuard");
+		}
+		File file = new File(absolutePath);
+		if (file.canRead())
+			return new FileResourceStream(file);
+		log.warn("Nonexistent theme file requested: {}", file);
+		return null;
+	}
+
+	/**
+	 * Determine if this file is OK to send.
+	 * @param path resource path
+	 * @return <code>true<code> if resource access is granted
+	 */
+	private boolean accessCheck (String path) {
+		IPackageResourceGuard guard = Application.get().getResourceSettings()
+				.getPackageResourceGuard();
+		return guard.accept(Application.class, path);
+	}
+
+	public Duration getCacheDuration () {
+		return cacheDuration;
+	}
+
+	public ThemeDirectoryRequestMapper setCacheDuration (Duration cacheDuration) {
+		this.cacheDuration = cacheDuration;
+		return this;
+	}
+
+
+}
