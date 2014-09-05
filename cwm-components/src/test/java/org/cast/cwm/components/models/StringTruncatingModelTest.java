a8213f57303f5aa6ac3129e45636e30a82b18afb
bgoldowsky <boris@alum.mit.edu>
2014-09-05 17:45:54 +0000

    New StringTruncatingModel for displaying strings that may be too long...

---
+++ cwm-components/src/test/java/org/cast/cwm/components/models/StringTruncatingModelTest.java
@@ -0,0 +1,130 @@
+package org.cast.cwm.components.models;
+
+import static org.junit.Assert.*;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringTruncatingModelTest {
+	
+	private IModel<String> delegate;
+	
+	@Before
+	public void setUp () {
+		delegate = new Model<String>("0123456789-123456789");
+	}
+	
+	@Test
+	public void passesThroughNullModelObject () {
+		delegate.setObject(null);
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		assertNull(model.getObject());
+	}
+
+	@Test
+	public void passesThroughEmptyString () {
+		delegate.setObject("");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		assertEquals("", model.getObject());
+	}
+
+	// Without word boundary detection
+	
+	@Test
+	public void rendersFullStringWhenLessThanMaxLength () {
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 25);
+		model.setBreakAtWordBoundary(false);
+		assertEquals(delegate.getObject(), model.getObject());
+	}
+	
+	@Test
+	public void rendersCorrectLengthWithDefaultSuffix () {
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10);
+		model.setBreakAtWordBoundary(false);
+		assertEquals("012345678…", model.getObject());
+	}
+	
+	@Test
+	public void rendersCorrectLengthWithSpecifiedSuffix () {
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		model.setBreakAtWordBoundary(false);
+		assertEquals("0123456...", model.getObject());
+	}
+	
+	@Test
+	public void normalizesSpaces () {
+		delegate.setObject("   one   two         three   four  five   ");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 13, "...");
+		model.setBreakAtWordBoundary(false);
+		assertEquals("one two th...", model.getObject());
+	}
+
+	@Test
+	public void normalizesWhitespace () {
+		delegate.setObject(" \t\n  one \r\r  two\t\t         three   four  five   ");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 13, "...");
+		model.setBreakAtWordBoundary(false);
+		assertEquals("one two th...", model.getObject());
+	}
+
+	// With word boundary detection
+	
+	@Test
+	public void wbRendersFullStringWhenLessThanMaxLength () {
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 25);
+		assertEquals(delegate.getObject(), model.getObject());
+	}
+		
+	@Test
+	public void wbFindsWordBoundary () {
+		delegate.setObject("one two three four five");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 13, "...");
+		assertEquals("one two ...", model.getObject());
+	}
+
+	@Test
+	public void wbNormalizesSpaces () {
+		delegate.setObject("   one   two         three   four  five   ");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 13, "...");
+		assertEquals("one two ...", model.getObject());
+	}
+
+	@Test
+	public void wbFindsLastWordBoundary () {
+		delegate.setObject("one two three four five");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		assertEquals("one two...", model.getObject());
+	}
+
+	@Test
+	public void wbDealsWithLongWords () {
+		delegate.setObject("one antidisestablishmentarianism");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		assertEquals("one ant...", model.getObject());
+	}
+
+	@Test
+	public void wbDealsWithNoWordBreaks () {
+		delegate.setObject("onetwothreefourfive");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		assertEquals("onetwot...", model.getObject());
+	}
+
+	@Test
+	public void wbFindsPunctuationBasedWordBoundary () {
+		delegate.setObject("one,two,three,four,five");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		assertEquals("one,two...", model.getObject());
+	}
+
+	@Test
+	public void wbFindsWhitespaceBasedWordBoundary () {
+		delegate.setObject("one\ntwo\nthree\nfour\nfive");
+		StringTruncatingModel model = new StringTruncatingModel(delegate, 10, "...");
+		assertEquals("one\ntwo...", model.getObject());
+	}
+
+
+}
