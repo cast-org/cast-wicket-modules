3788d51cf84473e5545c5fe7245442a23079442b
bgoldowsky@gmail.com <bgoldowsky@gmail.com@ff44da35-2226-727e-5b45-4a5c19511d84>
2014-09-05 17:47:22 +0000

    License header; more XPath tests.

--- trunk/cwm-components/src/test/java/org/cast/cwm/components/models/StringTruncatingModelTest.java
+++ trunk/cwm-components/src/test/java/org/cast/cwm/components/models/StringTruncatingModelTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011-2014 CAST, Inc.
+ *
+ * This file is part of the CAST Wicket Modules:
+ * see <http://code.google.com/p/cast-wicket-modules>.
+ *
+ * The CAST Wicket Modules are free software: you can redistribute and/or
+ * modify them under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The CAST Wicket Modules are distributed in the hope that they will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cast.cwm.components.models;
 
 import static org.junit.Assert.*;
@@ -43,7 +62,7 @@ public class StringTruncatingModelTest {
 	public void rendersCorrectLengthWithDefaultSuffix () {
 		StringTruncatingModel model = new StringTruncatingModel(delegate, 10);
 		model.setBreakAtWordBoundary(false);
-		assertEquals("012345678…", model.getObject());
+		assertEquals("012345678???", model.getObject());
 	}
 	
 	@Test
