01af80cea985ee8323aef4e0e6987dfa624e55f7
Boris Goldowsky <bgoldowsky@cast.org>
2023-01-17 08:42:15 -0500

    Add required copyright headers

--- cwm-lti/src/main/java/org/cast/cwm/lti/component/LinkingResponder.java
+++ cwm-lti/src/main/java/org/cast/cwm/lti/component/LinkingResponder.java
@@ -1,7 +1,25 @@
+/*
+ * Copyright 2011-2019 CAST, Inc.
+ *
+ * This file is part of the CAST Wicket Modules:
+ * see <http://code.google.com/p/cast-wicket-modules>.
+ *
+ * The CAST Wicket Modules are free software: you can redistribute and/or
+ * modify them under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The CAST Wicket Modules are distributed in the hope that they will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cast.cwm.lti.component;
 
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import com.google.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.wicket.Component;
@@ -30,7 +48,7 @@ public class LinkingResponder extends Panel {
     private final WebMarkupContainer form;
 
     private final WebMarkupContainer jwt;
-    
+
     public LinkingResponder(String id) {
         super(id);
 
--- cwm-lti/src/main/java/org/cast/cwm/lti/service/JwtValidationService.java
+++ cwm-lti/src/main/java/org/cast/cwm/lti/service/JwtValidationService.java
@@ -1,10 +1,33 @@
+/*
+ * Copyright 2011-2019 CAST, Inc.
+ *
+ * This file is part of the CAST Wicket Modules:
+ * see <http://code.google.com/p/cast-wicket-modules>.
+ *
+ * The CAST Wicket Modules are free software: you can redistribute and/or
+ * modify them under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The CAST Wicket Modules are distributed in the hope that they will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cast.cwm.lti.service;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.inject.Inject;
-import com.nimbusds.jose.*;
-import com.nimbusds.jose.jwk.*;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKMatcher;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.KeyConverter;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.jwk.source.RemoteJWKSet;
 import com.nimbusds.jose.proc.SecurityContext;
